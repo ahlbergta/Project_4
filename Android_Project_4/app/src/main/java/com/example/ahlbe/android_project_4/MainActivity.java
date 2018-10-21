5b61fc957a4b312f43d246f60f9e2a1e0854c012
Travis Ahlberg <ahlbergta@gmail.com>
2018-10-20 21:01:45 -0500

    Added beacon ranging in main activity

--- Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/MainActivity.java
+++ Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.ahlbe.android_project_4;
 
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+    private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+        beaconManager.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        Identifier conanNamespaceId = Identifier.parse("0x436f6e616e446f796c65");
+        Region region = new Region("conan-region", conanNamespaceId, null, null);
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+                while(collection.iterator().hasNext()) {
+                    Beacon beacon = collection.iterator().next();
+                }
+            }
+        });
+        try {
+            beaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
