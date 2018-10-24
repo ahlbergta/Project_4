cb6cc963530d5a5781d823720e031dfc868c0b42
Travis Ahlberg <ahlbergta@gmail.com>
2018-10-23 21:38:46 -0500

    Created main activity for navigation between the two demo activities

--- Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/MainActivity.java
+++ Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/MainActivity.java
@@ -1,61 +1,30 @@
 package com.example.ahlbe.android_project_4;
 
-import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
-import org.altbeacon.beacon.Beacon;
-import org.altbeacon.beacon.BeaconConsumer;
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Identifier;
-import org.altbeacon.beacon.RangeNotifier;
-import org.altbeacon.beacon.Region;
-
-import java.util.Collection;
-import java.util.Iterator;
-
-public class MainActivity extends AppCompatActivity implements BeaconConsumer {
-    private BeaconManager beaconManager;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
-        beaconManager.bind(this);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        beaconManager.unbind(this);
-    }
-
-    @Override
-    public void onBeaconServiceConnect() {
-        Identifier conanNamespaceId = Identifier.parse("0x436f6e616e446f796c65");
-        Region region = new Region("conan-region", conanNamespaceId, null, null);
-        beaconManager.addRangeNotifier(new RangeNotifier() {
+        Button notification_button = findViewById(R.id.notification_button);
+        Button ble_beacon_button = findViewById(R.id.ble_beacon_button);
+//        notification_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this, Notification_Activity.class));
+//            }
+//        });
+        ble_beacon_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
-                TextView textView = findViewById(R.id.textView);
-                String beaconInfo = "";
-                Iterator<Beacon> iterator = collection.iterator();
-                while(iterator.hasNext()) {
-                    Beacon beacon = iterator.next();
-                    beaconInfo += "Namespace: " + beacon.getId1().toString() + ", Identifier: " + beacon.getId2().toString() + "\n";
-                }
-                textView.setText(beaconInfo);
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BLE_Activity.class));
             }
         });
-        try {
-            beaconManager.startRangingBeaconsInRegion(region);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
     }
 }
