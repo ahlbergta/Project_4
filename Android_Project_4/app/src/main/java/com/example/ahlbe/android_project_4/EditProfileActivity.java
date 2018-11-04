50ecdc7b5e7ff070ea93d88f6fa24b66a03931be
Halscions <tho.le@ttu.edu>
2018-11-03 20:20:32 -0500

    Added some Edit Profile Functionality

--- Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/EditProfileActivity.java
+++ Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/EditProfileActivity.java
@@ -2,19 +2,42 @@ package com.example.ahlbe.android_project_4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import com.example.ahlbe.android_project_4.InputValidator.*;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class EditProfileActivity extends AppCompatActivity
 {
     private FirebaseAuth mAuth;
-    private EditText
+    private EditText mEmail, mPAddress, mSAddress, mFirstName, mLastName, mPPhone, mSPhone;
+    private Button mSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        //Set up all the widgets
+        mEmail = findViewById(R.id.edit_email);
+        mPAddress = findViewById(R.id.edit_p_address);
+        mSAddress = findViewById(R.id.edit_s_address);
+        mFirstName = findViewById(R.id.edit_first_name);
+        mLastName = findViewById(R.id.edit_last_name);
+        mPPhone = findViewById(R.id.edit_p_phone);
+        mSPhone = findViewById(R.id.edit_s_phone);
+
+        mSubmit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+
     }
+
 }
