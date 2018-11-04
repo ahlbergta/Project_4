63975725a88070debb0416cf1eb0739e8fb8f4e3
Halscions <tho.le@ttu.edu>
2018-11-03 19:39:10 -0500

    Changed XML String

--- Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/EditProfileActivity.java
+++ Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/EditProfileActivity.java
@@ -2,9 +2,14 @@ package com.example.ahlbe.android_project_4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EditProfileActivity extends AppCompatActivity
 {
+    private FirebaseAuth mAuth;
+    private EditText
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
