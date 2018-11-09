922d646079ed7ac0e3caf7d33cff53069606f036
Halscions <tho.le@ttu.edu>
2018-11-08 23:14:57 -0600

    Fixed the Register Activity the Delete Acitivty

--- Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/DeleteProfileConfirmationDialog.java
+++ Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/DeleteProfileConfirmationDialog.java
@@ -1,11 +1,13 @@
 package com.example.ahlbe.android_project_4;
 
+import android.app.Activity;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +16,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class DeleteProfileConfirmationDialog extends DialogFragment
 {
-    private static final String TAG = "DeleteProfileConfirmationDialog";
+    private static final String TAG = "DeleteProfileDialog";
 
     private Button mButtonConfirm, mButtonCancel;
     private Context mContext;
@@ -41,9 +47,29 @@ public class DeleteProfileConfirmationDialog extends DialogFragment
             @Override
             public void onClick(View view)
             {
+                Log.d(TAG, "Inside the onclick for deleting account");
+
+
                 FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
                 if(firebaseUser != null)
                 {
+                    Log.d(TAG, "Inside if firebase user is null:");
+                    DocumentReference documentReference = FirebaseFirestore.getInstance().collection("Users").document(firebaseUser.getUid());
+                    documentReference.delete().addOnSuccessListener(new OnSuccessListener<Void>()
+                    {
+                        @Override
+                        public void onSuccess(Void aVoid)
+                        {
+                            Log.d(TAG, "Document successfully deleted");
+                        }
+                    }).addOnFailureListener(new OnFailureListener()
+                    {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+                            Log.d(TAG, "Error deleting document");
+                        }
+                    });
                     firebaseUser.delete().addOnCompleteListener(new OnCompleteListener<Void>()
                     {
                         @Override
@@ -53,6 +79,11 @@ public class DeleteProfileConfirmationDialog extends DialogFragment
                             {
                                 Toast.makeText(mContext, "Profile Deleted Successfully", Toast.LENGTH_SHORT).show();
                                 getDialog().dismiss();
+                                Intent loginIntent = new Intent(mContext.getApplicationContext(), LoginActivity.class);
+                                loginIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(loginIntent);
+
+
                             }
                         }
                     });
