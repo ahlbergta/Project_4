8a74c91acf4dc2421bdd1998c5a2f34f22031888
Travis Ahlberg <ahlbergta@gmail.com>
2018-11-16 20:18:26 -0600

    Added foreground BLE scanning and subscription manager

--- Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/DemoApplication.java
+++ Android_Project_4/app/src/main/java/com/example/ahlbe/android_project_4/DemoApplication.java
@@ -1,13 +1,13 @@
 package com.example.ahlbe.android_project_4;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.RemoteException;
-import android.support.annotation.NonNull;
 import android.util.Log;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -20,33 +20,42 @@ import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class DemoApplication extends Application implements BootstrapNotifier, BeaconConsumer, RangeNotifier {
     private static final String ROCKET_ID = "0xd38dd9b09451";
     private static final String GROOT_ID = "0x4a72b2b79943";
+    private static final int CONAN_RANGING_NOTIFICATION_ID = 101;
     private RegionBootstrap regionBootstrap;
     private BackgroundPowerSaver backgroundPowerSaver;
     private BeaconManager beaconManager;
     private AlertManager alertManager;
+    private ArrayList<String> conanCache;
 
     @Override
     public void onCreate() {
         Log.d("BLE_Background_Scanner", "Start of onCreate");
         super.onCreate();
 
+        conanCache = new ArrayList<>();
+
         // Create the beacon manager and add Eddystone format to beacon parser
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
 
+        // Enable foreground BLE scanning by setting a notification
+        ForegroundRangingSetup();
+
         // Set beacon manager scan timing
+        beaconManager.setEnableScheduledScanJobs(false);
         beaconManager.setRegionStatePersistenceEnabled(false);
-        beaconManager.setForegroundBetweenScanPeriod(30001);   // 3000 ms
+        beaconManager.setForegroundBetweenScanPeriod(0);
         beaconManager.setForegroundScanPeriod(BeaconManager.DEFAULT_FOREGROUND_SCAN_PERIOD);
-        beaconManager.setBackgroundBetweenScanPeriod(30001);
+        beaconManager.setBackgroundBetweenScanPeriod(0);
         beaconManager.setBackgroundScanPeriod(BeaconManager.DEFAULT_FOREGROUND_SCAN_PERIOD);
 
-        beaconManager.setRangeNotifier(this);
+        beaconManager.addRangeNotifier(this);
 
         // Create the conan region
         Identifier conanNamespaceId = Identifier.parse("0x436f6e616e446f796c65");
@@ -57,32 +66,38 @@ public class DemoApplication extends Application implements BootstrapNotifier, B
 
         backgroundPowerSaver = new BackgroundPowerSaver(this);
 
-        // TEST CODE
+        // ------------------ TEST CODE
         // Subscribe to conanID
-//        subscribe(ROCKET_ID);
-//        subscribe(GROOT_ID);
+        SubscriptionManager.subscribe(GROOT_ID);
+        SubscriptionManager.subscribe(ROCKET_ID);
+        // ------------------ END TEST CODE
 
         Log.d("BLE_Background_Scanner", "End of onCreate");
     }
 
-    private void subscribe(String id){
-        FirebaseMessaging.getInstance().subscribeToTopic(id)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-//                        String msg = getString(R.string.msg_subscribed);
-//                        if (!task.isSuccessful()) {
-//                            msg = getString(R.string.msg_subscribe_failed);
-//                        }
-                        Log.d("Subscribe", "In subscription callback listener");
-                    }
-                });
+    private void ForegroundRangingSetup(){
+        Log.d("BLE_Background_Scanner", "In ForegroundRangingSetup");
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        builder.setContentTitle("Scanning for Conan beacons");
+
+        // Create notification channel if version is Oreo or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Conan Ranging Channel",
+                    "Conan ranging notification channel", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Channel for notifying users of background BLE Conan scanning");
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channel.getId());
+        }
+        beaconManager.enableForegroundServiceScanning(builder.build(), CONAN_RANGING_NOTIFICATION_ID);
     }
 
     @Override
     public void didEnterRegion(Region region) {
-        Log.d("BLE_Background_Scanner", "Conan beacon found: " + region.getId2());
+        Log.d("BLE_Background_Scanner", "Conan beacon found");
         try{
+            //Start background scanning service
             beaconManager.startRangingBeaconsInRegion(region);
         } catch(RemoteException e) {
             e.printStackTrace();
@@ -102,26 +117,44 @@ public class DemoApplication extends Application implements BootstrapNotifier, B
     @Override
     public void didDetermineStateForRegion(int i, Region region) {
         Log.d("BLE_Background_Scanner", "In didDetermineStateForRegion, current state: " + i);
-//        if(i==1){
-//            didEnterRegion(region);
-//        }
-//        if(i==0){
-//            didExitRegion(region);
-//        }
     }
 
     @Override
     public void onBeaconServiceConnect() {
         Log.d("BLE_Background_Scanner", "Beacon service ready");
-//        beaconManager.setRangeNotifier(this);
     }
 
     @Override
     public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
         // Code for when Conan device is ranged
         Log.d("BLE_Background_Scanner", "Start beacon range callback");
+
+        // Remove beacon from collection if it is in the cache, otherwise add it to the cache
+        ArrayList<String> newCache = new ArrayList<>();
+        for(Beacon beacon : collection) {
+            String id = beacon.getId2().toHexString();
+
+            // Only keep cached ids that are in the collection
+            if(conanCache.contains(id)) {
+                // Remove beacons from the collection that are in the cache, add the id to the updated cache
+                Log.d("BLE_Background_Scanner", "Cached beacon found: " + id);
+                newCache.add(id);
+                collection.remove(beacon);
+            }
+            else {
+                // Add a new beacon to the cache
+                Log.d("BLE_Background_Scanner", "New beacon found: " + id);
+                newCache.add(id);
+            }
+        }
+        conanCache = newCache;
+
+        // If there are any beacons in the collection create alerts for them
         if(collection.size() > 0){
             alertManager = new AlertManager(this.getApplicationContext(), collection);
+        }
+        else {
+
         }
     }
 }
